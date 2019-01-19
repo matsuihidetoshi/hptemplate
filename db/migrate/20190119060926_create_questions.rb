3b25cd0d0496a6fed99f590aa4752685ad95a232
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-01-19 15:10:59 +0900

    git init

---
+++ db/migrate/20190119060926_create_questions.rb
@@ -0,0 +1,14 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :content
+      t.string :name
+      t.string :address
+      t.string :tel
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
