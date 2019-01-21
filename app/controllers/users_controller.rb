23abf6cd746f1f9de8cb9e4c06264cef8714f2d2
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-01-21 20:15:01 +0900

    configured kaminari, created users controller, created sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,63 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in,
+  only: [
+    :index, :show, :new, :create,
+    :edit, :update, :destroy
+  ]
+
   def index
+    @users = User.all.page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '管理者を登録しました。'
+      redirect_to users_path
+    else
+      flash[:danger] = '管理者を登録できませんでした。'
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = '管理者情報を更新しました。'
+      redirect_to @user
+    else
+      flash[:danger] = '管理者情報を更新できませんでした。'
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = '管理者を削除しました。'
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.
+    require(:user)
+    .permit(
+      :name, :email, :password,
+      :password_confirmation
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
   resources :users
   resources :questions
