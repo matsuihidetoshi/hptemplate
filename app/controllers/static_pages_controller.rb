472de4098cc2fa2f564b585d856c9e2690b3adcd
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-01-26 20:47:01 +0900

    add footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def company
+  end
+
+  def service
+  end
+
+  def access
+  end
+
+  def careers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
+  get 'company', to: 'static_pages#company'
+  get 'service', to: 'static_pages#service'
+  get 'access', to: 'static_pages#access'
+  get 'careers', to: 'static_pages#careers'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
